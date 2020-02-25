fc63ea855ef6f1da67af6ea6a39e44130fcb011a
Robbe Motmans <robbemotmans@hotmail.com>
2020-02-25 22:47:06 +0100

    state variables, constructor and fallback function

--- contracts/OneEtherToken.sol
+++ contracts/OneEtherToken.sol
@@ -7,4 +7,26 @@ contract OneEtherToken is ERC20Interface{
 
   using SafeMath for uint;
 
+  string public symbol;
+  string public  name;
+  uint8 public decimals;
+
+  uint256 private _totalSupply;
+
+  mapping(address => uint256) private _balances;
+  mapping(address => mapping(address => uint256)) private _allowances;
+
+  //Contructor
+  constructor() public {
+      symbol = "OneETH";
+      name = "OneEther";
+      decimals = 18;
+      _totalSupply = 0;
+  }
+
+
+  fallback() external payable{
+    _balances[msg.sender] = msg.value;
+    _totalSupply += msg.value;
+  }
 }
